67b0cdaf976cdc92fe7bb459ff762b38f19c6940
daFoggo <ntgiang141105@gmail.com>
2025-02-21 03:45:42 +0700

    modify page loader

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,13 @@ import { ThemeProvider } from "./components/common/ThemeProvider";
 import { AuthProvider } from "./contexts/AuthContext";
 import "./App.css";
 import { router } from "./routes";
+import PageLoader from "./components/common/PageLoader";
 
 const App = () => {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <AuthProvider>
-        <Suspense fallback={<div>Loading...</div>}>
+        <Suspense fallback={<PageLoader />}>
           <RouterProvider router={router} />
         </Suspense>
       </AuthProvider>
